58614e8708e064fda3dba43d84363c2cb615c053
lucashenriquecss <calheirossaraiva@hotmail.com>
2021-08-30 09:23:15 -0300

    att testes

--- setup/tests.py
+++ setup/tests.py
@@ -4,4 +4,14 @@ from selenium import webdriver
 class AnimaisTestCase(LiveServerTestCase):
 
     def setUp(self):
-        pass
+        self.browser = webdriver.Chrome('/Users/calhe/Documents/GitHub/TDD_django/chromedriver.exe')
+
+    def tearDown(self):#fechar aba do chrome ao fazer todos od testes
+        self.browser.quit()
+
+    def open_windowbrowser(self):
+        self.browser.get(self.live_server_url)
+
+    def test_de_falha(self):
+        """ Exemplo de teste de erro"""
+        self.fail('Teste falhou!!')
